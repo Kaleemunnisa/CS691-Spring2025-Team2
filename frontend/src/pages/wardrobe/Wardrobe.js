bb75c027552c39b35f3e8a25deb320c9466537a7
bolanos7 <cristianbolanos332@gmail.com>
2025-03-06 00:09:54 -0500

    Fixed minor css

--- frontend/src/pages/wardrobe/Wardrobe.js
+++ frontend/src/pages/wardrobe/Wardrobe.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ClothingList from "../../components/clothing-list/ClothingList";
-import "./Wardrobe.css";
 import HeaderBar from "../../components/header/HeaderBar";
 
 function Wardrobe() {
   return (
-    <div className="wardrobe-container">
+    <div>
       <HeaderBar />
-
       <ClothingList />
     </div>
   );
